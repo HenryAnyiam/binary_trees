1477b3bae82220daced489509ad5e8320215a1ec
HenryAnyiam <anyiamhenry7@gmail.com>
2023-08-01 17:07:26 -0700

    knowing the binary tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@
  */
 int check_tree(const binary_tree_t *tree)
 {
-	int left, right;
+	int left, right, check;
 
 	if (tree == NULL)
 		return (-1);
@@ -16,6 +16,9 @@ int check_tree(const binary_tree_t *tree)
 	right = check_tree(tree->right);
 	if (left > right)
 		return (left + 1);
+	check = binary_tree_is_perfect(tree);
+	if (check == 0)
+		return (left + right + 1);
 	return (right + 1);
 }
 
